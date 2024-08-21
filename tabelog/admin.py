52893058dc07f122e202e3d6a59c987182067190
tatene9912 <t.atene0l4o1v2e@gmail.com>
2024-08-21 22:41:49 +0900

    already

--- tabelog/admin.py
+++ tabelog/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Locations, Categories
+from .models import Locations, Categories, Holidays
 from django.utils.safestring import mark_safe
 
 class LocationsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'category', 'image')
+    list_display = ('id', 'name', 'category', 'holiday', 'image', 'description', 'capacity', 'postal_code', 'address', 'phonenumber', 'price_low', 'price_high', 'time_open', 'time_close', 'created_date', 'updated_date')
 
     def image(self, obj):
         return mark_safe('<img src="{}" style="width:100px height:auto;">'.format(obj.img.url))
@@ -12,5 +12,10 @@ class CategoriesAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
     search_fields = ('name', )
 
+class HolidaysAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name', )
+
 admin.site.register(Categories)
+admin.site.register(Holidays)
 admin.site.register(Locations, LocationsAdmin)
--- tabelog/models.py
+++ tabelog/models.py
@@ -2,6 +2,16 @@ from django.db import models
 
 class Categories(models.Model):
     name = models.CharField(max_length=20, verbose_name='カテゴリ名')
+    created_date = models.TimeField(auto_now_add=True, null=True)
+    updated_date = models.TimeField(auto_now=True, null=True)
+
+    def __str__(self):
+        return self.name
+    
+class Holidays(models.Model):
+    name = models.CharField(max_length=50, verbose_name='休日名')
+    created_date = models.TimeField(auto_now_add=True)
+    updated_date = models.TimeField(auto_now=True)
 
     def __str__(self):
         return self.name
@@ -9,6 +19,7 @@ class Categories(models.Model):
 class Locations(models.Model):
     name = models.CharField(max_length=50, verbose_name='店名')
     category = models.ForeignKey(Categories, on_delete=models.CASCADE, verbose_name='カテゴリ')
+    holiday = models.ForeignKey(Holidays, on_delete=models.CASCADE, verbose_name='店休日', null=True)
     image = models.ImageField(blank=True, default='noImage.png', verbose_name='画像')
     description = models.TextField(max_length=200, verbose_name='説明', null=True)
     capacity = models.IntegerField(verbose_name='定員', null=True)
